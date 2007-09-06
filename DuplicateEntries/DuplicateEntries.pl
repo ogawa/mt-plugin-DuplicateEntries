d04c67b2cc171d8903f11560bd82d199a7daa69e
Hirotaka Ogawa <hirotaka.ogawa@gmail.com>
2007-09-06 13:08:00 +0000

    modify plugin description.
    git-svn-id: file:///Users/ogawa/svn/mt-plugin-DuplicateEntries/trunk@4 b7bee0e5-6241-4529-aa9e-08ae86e46850

--- DuplicateEntries/DuplicateEntries.pl
+++ DuplicateEntries/DuplicateEntries.pl
@@ -21,7 +21,7 @@ our $VERSION = '0.01';
 my $plugin = __PACKAGE__->new({
     id => 'duplicate_entries',
     name => 'DuplicateEntries',
-    description => q(<MT_TRANS phrase="A plugin for building Fiscal Yearly Archives">),
+    description => q(<MT_TRANS phrase="DuplicateEntries plugin allows you to duplicate Movable Type entries and pages.">),
     doc_link => 'http://code.as-is.net/wiki/DuplicateEntries',
     author_name => 'Hirotaka Ogawa',
     author_link => 'http://profile.typekey.com/ogawa/',
--- DuplicateEntries/lib/DuplicateEntries/L10N/ja.pm
+++ DuplicateEntries/lib/DuplicateEntries/L10N/ja.pm
@@ -6,6 +6,7 @@ use base 'DuplicateEntries::L10N';
 use vars qw( %Lexicon );
 
 our %Lexicon = (
+    'DuplicateEntries plugin allows you to duplicate Movable Type entries and pages.' => 'DuplicateEntriesプラグインを用いると、Movable Typeのブログ記事・ウェブページを複製することができます。',
     'Duplicate Entries' => 'ブログ記事の複製',
     'Duplicate Pages' => 'ウェブページの複製',
 );
